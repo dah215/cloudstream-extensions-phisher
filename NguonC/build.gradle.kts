73a9971ae650fe46de1fe7562cc081e07d9273da
dah215 <duongah514hp@gmail.com>
2026-01-27 20:48:09 +0700

    Add files via upload

---
+++ NguonC/build.gradle.kts
@@ -0,0 +1,22 @@
+version = 10
+
+cloudstream {
+
+    description = "Aho (Lang) NguonC"
+    language    = "vi"
+    authors = listOf("Aho")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 
+    
+    tvTypes = listOf("NguonC")
+    iconUrl = "https://phim.nguonc.com/public/images/Logo/logonc.png"
+
+    isCrossPlatform = true
+}
