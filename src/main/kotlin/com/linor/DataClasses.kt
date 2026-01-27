43d58383cce4d2bba46f31abd32b64fb58bb435f
dah215 <duongah514hp@gmail.com>
2026-01-27 19:29:53 +0700

    Create DataClasses.kt

---
+++ src/main/kotlin/com/linor/DataClasses.kt
@@ -0,0 +1,72 @@
+package com.linor
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.Actor
+
+data class ListResponse(@JsonProperty("items") val items: List<MoviesResponse>)
+
+data class MoviesResponse(
+    @JsonProperty("name") val name: String?,
+    @JsonProperty("slug") val slug: String,
+    @JsonProperty("thumb_url") val thumbUrl: String?,
+    @JsonProperty("poster_url") val posterUrl: String?,
+    @JsonProperty("quality") val quality: String?,
+    @JsonProperty("language") val language: String?,
+    @JsonProperty("current_episode") val episode: String?
+)
+
+data class MovieInfo(@JsonProperty("movie") val movie: MovieDetailResponse)
+
+data class MovieDetailResponse(
+    @JsonProperty("name") val name: String,
+    @JsonProperty("slug") val slug: String,
+    @JsonProperty("content") val content: String,
+    @JsonProperty("thumb_url") val thumbUrl: String,
+    @JsonProperty("poster_url") val posterUrl: String,
+    @JsonProperty("casts") val casts: String?,
+    @JsonProperty("category") val category: MovieCategoryResponse,
+    @JsonProperty("episodes") val episodes: List<MovieEpisodeResponse>,
+    @JsonProperty("total_episodes") val episode_total: Int?,
+    @JsonProperty("original_name") val origin_name: String?,
+    @JsonProperty("time") val time: String?,
+    @JsonProperty("vote_average") val voteAverage: Double?
+)
+
+data class MovieCategoryResponse(
+    @JsonProperty("2") val category2: MovieCategoryItemResponse,
+    @JsonProperty("3") val category3: MovieCategoryItemResponse,
+    @JsonProperty("4") val category4: MovieCategoryItemResponse
+)
+
+data class MovieCategoryItemResponse(
+    @JsonProperty("list") val list: List<MovieCategoryListResponse>
+)
+
+data class MovieCategoryListResponse(
+    @JsonProperty("name") val name: String
+)
+
+data class MovieEpisodeResponse(
+    @JsonProperty("server_name") val serverName: String,
+    @JsonProperty("items") val serverData: List<EpisodeData>
+)
+
+data class EpisodeData(
+    @JsonProperty("name") val name: String,
+    @JsonProperty("slug") val slug: String,
+    @JsonProperty("m3u8") val linkM3u8: String,
+    @JsonProperty("embed") val linkEmbed: String
+)
+
+data class Lazy(
+    val backdrop: String?,
+    val content: String?,
+    val cast: List<Actor>?,
+    val genres: List<String>?,
+    val vote: Int?,
+    val runtime: Int?,
+    val year: Int?,
+    val country: String?
+)
+
+data class ParsedTitle(val title: String, val season: String)
