eda5956bcb95de7f8741c7ce8defa4b4c726a93a
dah215 <duongah514hp@gmail.com>
2026-01-28 12:48:41 +0700

    Update PhimNguonCPlugin.kt

--- NguonC/src/main/kotlin/com/linor/PhimNguonCPlugin.kt
+++ NguonC/src/main/kotlin/com/linor/PhimNguonCPlugin.kt
@@ -1,11 +1,11 @@
 package com.linor
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class PhimNguonCPlugin: Plugin() {
-    override fun load(context: Context) {
+    override fun load(context: android.content.Context) {
+        // Không cần import, viết trực tiếp android.content.Context ở trên là được
         registerMainAPI(PhimNguonCProvider(this))
     }
 }
