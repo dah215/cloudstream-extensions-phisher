1e4a1f2f6b2fb46faa3020ba1113745db04b105a
dah215 <duongah514hp@gmail.com>
2026-01-28 22:06:17 +0700

    Update DataClasses.kt

--- NguonC/src/main/kotlin/com/linor/DataClasses.kt
+++ NguonC/src/main/kotlin/com/linor/DataClasses.kt
@@ -3,43 +3,40 @@ package com.linor
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ListResponse(
-    @JsonProperty("items") val items: List<MoviesResponse>?
+    @JsonProperty("items") val items: List<MoviesResponse>? = null
 )
 
 data class MoviesResponse(
-    @JsonProperty("name") val name: String?,
-    @JsonProperty("slug") val slug: String?,
-    @JsonProperty("thumb_url") val thumbUrl: String?,
-    @JsonProperty("poster_url") val posterUrl: String?,
-    @JsonProperty("episode_current") val episode: String?
+    @JsonProperty("name") val name: String? = null,
+    @JsonProperty("slug") val slug: String? = null,
+    @JsonProperty("thumb_url") val thumbUrl: String? = null,
+    @JsonProperty("poster_url") val posterUrl: String? = null,
+    @JsonProperty("episode_current") val episode: String? = null
 )
 
 data class MovieInfo(
-    @JsonProperty("movie") val movie: MovieDetailResponse?,
-    @JsonProperty("episodes") val episodes: List<MovieEpisodeResponse>?
+    @JsonProperty("movie") val movie: MovieDetailResponse? = null,
+    @JsonProperty("episodes") val episodes: List<MovieEpisodeResponse>? = null
 )
 
 data class MovieDetailResponse(
-    @JsonProperty("name") val name: String?,
-    @JsonProperty("origin_name") val originName: String?,
-    @JsonProperty("content") val content: String?,
-    @JsonProperty("type") val type: String?,
-    @JsonProperty("thumb_url") val thumbUrl: String?,
-    @JsonProperty("poster_url") val posterUrl: String?,
-    @JsonProperty("year") val year: Int?,
-    @JsonProperty("casts") val casts: String?
+    @JsonProperty("name") val name: String? = null,
+    @JsonProperty("slug") val slug: String? = null,
+    @JsonProperty("content") val content: String? = null,
+    @JsonProperty("type") val type: String? = null,
+    @JsonProperty("thumb_url") val thumbUrl: String? = null,
+    @JsonProperty("poster_url") val posterUrl: String? = null,
+    @JsonProperty("year") val year: Int? = null,
+    @JsonProperty("casts") val casts: String? = null
 )
 
 data class MovieEpisodeResponse(
-    @JsonProperty("server_name") val serverName: String?,
-    @JsonProperty("server_data") val serverData: List<EpisodeData>?
+    @JsonProperty("server_name") val serverName: String? = null,
+    @JsonProperty("server_data") val serverData: List<EpisodeData>? = null
 )
 
 data class EpisodeData(
-    @JsonProperty("name") val name: String?,
-    @JsonProperty("slug") val slug: String?,
-    @JsonProperty("link_m3u8") val linkM3u8: String?,
-    @JsonProperty("link_embed") val linkEmbed: String?
+    @JsonProperty("name") val name: String? = null,
+    @JsonProperty("link_m3u8") val linkM3u8: String? = null,
+    @JsonProperty("link_embed") val linkEmbed: String? = null
 )
-
-data class ParsedTitle(val title: String, val season: String)
