af3712ae9ab85320ea60baccdd063293eb74982e
dah215 <duongah514hp@gmail.com>
2026-01-28 21:42:46 +0700

    Update FunctionHelpKt.kt

--- NguonC/src/main/kotlin/com/linor/FunctionHelpKt.kt
+++ NguonC/src/main/kotlin/com/linor/FunctionHelpKt.kt
@@ -1,22 +1,23 @@
 package com.linor
 
 import com.lagradost.cloudstream3.network.CloudflareKiller
-import java.net.URI
 
 object FunctionHelpKt {
     var mainUrlImage = "https://phim.nguonc.com"
     val cfKiller = CloudflareKiller()
 
-    fun getImageUrl(url: String): String {
+    fun getImageUrl(url: String?): String {
+        if (url == null) return ""
         return if (url.startsWith("/")) "$mainUrlImage$url" else url
     }
 
-    fun parseTitleAndSeason(input: String): ParsedTitle {
+    fun parseTitleAndSeason(input: String?): ParsedTitle {
+        val name = input ?: ""
         val seasonRegex = Regex("season\\s*(\\d+)", RegexOption.IGNORE_CASE)
-        val seasonMatch = seasonRegex.find(input)
+        val seasonMatch = seasonRegex.find(name)
         val season = seasonMatch?.groupValues?.get(1)?.let { "Season $it" } ?: "Season 1"
         
-        val title = input.replace(Regex("season\\s*\\d+", RegexOption.IGNORE_CASE), "")
+        val title = name.replace(Regex("season\\s*\\d+", RegexOption.IGNORE_CASE), "")
                          .replace(Regex("\\b(Movie|Remake|OVA)\\b", RegexOption.IGNORE_CASE), "")
                          .trim()
         return ParsedTitle(title, season)
