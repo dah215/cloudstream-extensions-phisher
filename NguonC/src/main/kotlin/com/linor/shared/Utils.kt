12a09f46ce9071f637a142b7bfe1644317f7685f
dah215 <duongah514hp@gmail.com>
2026-01-28 12:49:08 +0700

    Update Utils.kt

--- NguonC/src/main/kotlin/com/linor/shared/Utils.kt
+++ NguonC/src/main/kotlin/com/linor/shared/Utils.kt
@@ -1,11 +1,16 @@
 package com.linor.shared
 
 object Utils {
-    fun countWords(text: String): Int {
+    fun countWords(text: String?): Int {
+        if (text == null) return 0
         return text.trim().split("\\s+".toRegex()).size
     }
 
-    fun formatImdb(score: Double?, useFiveStars: Boolean): String {
+    fun formatImdb(score: Any?, useFiveStars: Boolean): String {
         return score?.toString() ?: ""
+    }
+
+    fun formatYear(year: Int?): String {
+        return if (year != null) " ($year)" else ""
     }
 }
